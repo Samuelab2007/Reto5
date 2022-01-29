853b42af7b43baacdfc9d36778552ef138d91e8a
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2022-01-29 17:38:47 -0500

    Métodos y atributos no abstractos en Pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,5 +1,26 @@
 public abstract class Pokemon {
 
+    private String nombre;
+    private byte nivel;
+    private int salud;
+    private int tamanho;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public byte getNivel() {
+        return nivel;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
     public abstract Pokemon evolucionar() throws NoEvolucionaException;
     public abstract String gritar();
 }
