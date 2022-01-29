d4b530f9217b6aac5ec4ba7e1e9f6a272e0bfa6a
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2022-01-29 17:38:05 -0500

    Método capturar en "Pokebola.java"

--- src/Pokebola.java
+++ src/Pokebola.java
@@ -1,20 +1,36 @@
 class Pokebola {
-    private String tamanho;
+    private final int tamanho;
     private Pokemon pokemonCapturado;
+    private byte pokemonAdentro;
 
-    public String gettamanho() {
-        return tamanho;
-    }
-
-    public void settamanho(String tamanho) {
+    public Pokebola(int tamanho) {
         this.tamanho = tamanho;
     }
+
 
     public Pokemon getPokemonCapturado() {
         return pokemonCapturado;
     }
 
+    public byte getpokemonAdentro() {
+        return pokemonAdentro;
+    }
+
     public void setPokemonCapturado(Pokemon pokemonCapturado) {
         this.pokemonCapturado = pokemonCapturado;
     }
+
+    public String capturar(Pokemon pokemonCapturado){
+        if(this.tamanho >= pokemonCapturado.getTamanho()){
+            setPokemonCapturado(pokemonCapturado);
+            pokemonAdentro = 1;
+            return "Capturado";
+
+        }else{
+            pokemonAdentro = 0;
+            return "Demasiado grande para capturar";
+        }
+
+    }
+
 }
